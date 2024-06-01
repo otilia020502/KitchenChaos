582682df887871c7a13b169d26dfc1edcd232b6a
Otilia Dudau <otiliadudau0205@gmail.com>
2024-06-01 10:57:07 +0300

    Second class end,before homework

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    {SerializedField}private float moveSpeed = 5f;
+    
     void Update()
     {
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.x = +1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.y = +1f;
+        }
+
+        inputVector = inputVector.normalized;
+        Vector3 movedir = new Vector3(inputVector.x, 0f, inputVector.y);
         
     }
 }
