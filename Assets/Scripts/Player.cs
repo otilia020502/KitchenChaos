61358ee375d4258cbe49547c25a7fee40815549a
Otilia Dudau <otiliadudau0205@gmail.com>
2024-06-04 10:18:17 +0300

    added new input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,30 +3,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private GameInput _gameInput;
     private bool isWalking;
 
     void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1f;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1f;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1f;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1f;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = _gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         isWalking = moveDir != Vector3.zero;
         // Multiply moveSpeed with Time.deltaTime first for better performance
