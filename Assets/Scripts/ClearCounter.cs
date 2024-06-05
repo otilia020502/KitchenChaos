66951bfd7aaaa83aa4e6252a39a677a8e19e11fa
Otilia Dudau <otiliadudau0205@gmail.com>
2024-06-05 13:01:04 +0300

    Day 5 done

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
-  
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private GameObject objectToSpan;
     public void Interact()
     {
-        Debug.Log("Interact!");
+        var spannedObject=Instantiate(objectToSpan, counterTopPoint);
+        spannedObject.transform.localPosition = Vector3.zero;
     }
    
 }
