f394027f187ebc2c9831dbde4df13c0862e5a4cb
Otilia Dudau <otiliadudau0205@gmail.com>
2024-06-19 22:59:34 +0300

    cameras work perfectly

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    public RecipeSO recipe;
+    public Time arrivalTime;
+
+    public void GetWaitingTime()
+    {
+        
+    }
+    
+}
